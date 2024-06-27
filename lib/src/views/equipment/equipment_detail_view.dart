071705703b6580f0f521b0a9656e6c8e7d3c23ee
duncanfraiser <dfraiserjr@gmail.com>
2024-06-27 03:20:50 -0400

    equipment and material styles

--- lib/src/views/equipment/equipment_detail_view.dart
+++ lib/src/views/equipment/equipment_detail_view.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:lazarus_job_tracker/src/models/equipment_model.dart';
+import 'package:lazarus_job_tracker/src/services/equipment_service.dart';
+import 'package:lazarus_job_tracker/src/services/auth_service.dart';
+import 'package:lazarus_job_tracker/src/models/user_model.dart';
+import 'package:provider/provider.dart';
+import 'package:lazarus_job_tracker/src/app_styles.dart';
 import 'package:lazarus_job_tracker/src/views/equipment/equipment_create_update_view.dart';
+import 'package:lazarus_job_tracker/src/widgets/reusable_card.dart';
+import 'package:lazarus_job_tracker/src/widgets/loading_view.dart';
+import 'package:lazarus_job_tracker/src/widgets/error_view.dart';
+import 'package:lazarus_job_tracker/src/widgets/no_data_view.dart';
+import 'package:lazarus_job_tracker/src/widgets/top_bar.dart';
+import 'package:lazarus_job_tracker/src/widgets/bottom_bar.dart';
 
 class EquipmentDetailView extends StatelessWidget {
   final EquipmentModel equipment;
@@ -9,63 +20,124 @@ class EquipmentDetailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(equipment.name),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            Text('Equipment Details', style: Theme.of(context).textTheme.bodyLarge),
-            const SizedBox(height: 16.0),
-            Card(
-              elevation: 4.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+    final equipmentService = Provider.of<EquipmentService>(context, listen: false);
+    final authService = Provider.of<AuthService>(context, listen: false);
+
+    void navigateHome() {
+      Navigator.popUntil(context, ModalRoute.withName('/'));
+    }
+
+    return FutureBuilder<UserModel?>(
+      future: authService.getUserData(authService.currentUser!.uid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingView(title: 'Loading...');
+        }
+
+        if (snapshot.hasError) {
+          return ErrorView(title: 'Error', errorMessage: snapshot.error.toString());
+        }
+
+        if (!snapshot.hasData) {
+          return const NoDataView(title: 'Equipment Detail', message: 'No user data found');
+        }
+
+        final user = snapshot.data!;
+        return Scaffold(
+          appBar: TopBar(
+            companyName: user.companyName,
+            userName: '${user.firstName} ${user.lastName}',
+            actions: [
+              IconButton(
+                icon: Icon(Icons.edit, color: AppStyles.orangeEditIcon.color, size: AppStyles.orangeEditIcon.size),
+                iconSize: AppStyles.topBarIconSize,
+                color: AppStyles.iconTheme.color,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EquipmentCreateUpdateView(equipment: equipment),
+                    ),
+                  );
+                },
               ),
-              child: ListTile(
-                title: const Text('Name'),
-                subtitle: Text(equipment.name),
+              IconButton(
+                icon: Icon(Icons.delete, color: AppStyles.redDeleteIcon.color, size: AppStyles.redDeleteIcon.size),
+                iconSize: AppStyles.topBarIconSize,
+                color: AppStyles.iconTheme.color,
+                onPressed: () async {
+                  final confirmDelete = await showDialog<bool>(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Center(
+                        child: Text('Delete Equipment'),
+                      ),
+                      content: Text('Are you sure you want to delete ${equipment.name} from the equipment list?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(false),
+                          child: const Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(true),
+                          child: const Text('Delete'),
+                        ),
+                      ],
+                    ),
+                  );
+
+                  if (confirmDelete == true) {
+                    await equipmentService.deleteEquipment(equipment.documentId!);
+                    Navigator.pop(context); // Close the detail view after deletion
+                  }
+                },
               ),
-            ),
-            const SizedBox(height: 8.0),
-            Card(
-              elevation: 4.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: ListTile(
-                title: const Text('Description'),
-                subtitle: Text(equipment.description),
-              ),
-            ),
-            const SizedBox(height: 8.0),
-            Card(
-              elevation: 4.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: ListTile(
-                title: const Text('Rate per Hour'),
-                subtitle: Text('\$${equipment.ratePerHour.toStringAsFixed(2)}'),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => EquipmentCreateUpdateView(equipment: equipment),
+            ],
+          ),
+          backgroundColor: AppStyles.backgroundColor,
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 8.0), // Reduced spacing
+                  child: Text(
+                    'Equipment Detail',
+                    style: AppStyles.headlineStyle,
                   ),
-                );
-              },
-              child: const Text('Edit Equipment'),
+                ),
+                const SizedBox(height: 8.0), // Reduced spacing
+                Expanded(
+                  child: ListView(
+                    children: [
+                      ReusableCard(
+                        icon: Icons.build,
+                        title: 'Name',
+                        subtitle: equipment.name,
+                      ),
+                      ReusableCard(
+                        icon: Icons.description,
+                        title: 'Description',
+                        subtitle: equipment.description,
+                      ),
+                      ReusableCard(
+                        icon: Icons.attach_money,
+                        title: 'Rate per Hour',
+                        subtitle: '\$${equipment.ratePerHour.toStringAsFixed(2)}',
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+          bottomNavigationBar: BottomBar(
+            onHomePressed: navigateHome,
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        );
+      },
     );
   }
 }
--- lib/src/views/job_material/job_material_detail_view.dart
+++ lib/src/views/job_material/job_material_detail_view.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:lazarus_job_tracker/src/models/job_material_model.dart';
+import 'package:lazarus_job_tracker/src/services/job_material_service.dart';
+import 'package:lazarus_job_tracker/src/services/auth_service.dart';
+import 'package:lazarus_job_tracker/src/models/user_model.dart';
+import 'package:provider/provider.dart';
+import 'package:lazarus_job_tracker/src/app_styles.dart';
 import 'package:lazarus_job_tracker/src/views/job_material/job_material_create_update_view.dart';
+import 'package:lazarus_job_tracker/src/widgets/reusable_card.dart';
+import 'package:lazarus_job_tracker/src/widgets/loading_view.dart';
+import 'package:lazarus_job_tracker/src/widgets/error_view.dart';
+import 'package:lazarus_job_tracker/src/widgets/no_data_view.dart';
+import 'package:lazarus_job_tracker/src/widgets/top_bar.dart';
+import 'package:lazarus_job_tracker/src/widgets/bottom_bar.dart';
 
 class JobMaterialDetailView extends StatelessWidget {
   final JobMaterialModel jobMaterial;
@@ -9,62 +20,124 @@ class JobMaterialDetailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(jobMaterial.name),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            _buildCard('Name', jobMaterial.name),
-            const SizedBox(height: 8.0),
-            _buildCard('Price', '\$${jobMaterial.price.toStringAsFixed(2)}'),
-            const SizedBox(height: 8.0),
-            _buildCard('Description', jobMaterial.description),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => JobMaterialCreateUpdateView(jobMaterial: jobMaterial),
-                  ),
-                );
-              },
-              child: const Text('Edit Material'),
-            ),
-            const SizedBox(height: 8.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const JobMaterialCreateUpdateView(),
-                  ),
-                );
-              },
-              child: const Text('Create New Material'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+    final jobMaterialService = Provider.of<JobMaterialService>(context, listen: false);
+    final authService = Provider.of<AuthService>(context, listen: false);
 
-  Widget _buildCard(String title, String content) {
-    return Card(
-      elevation: 4.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: ListTile(
-        title: Text(
-          title,
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        subtitle: Text(content),
-      ),
+    void navigateHome() {
+      Navigator.popUntil(context, ModalRoute.withName('/'));
+    }
+
+    return FutureBuilder<UserModel?>(
+      future: authService.getUserData(authService.currentUser!.uid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingView(title: 'Loading...');
+        }
+
+        if (snapshot.hasError) {
+          return ErrorView(title: 'Error', errorMessage: snapshot.error.toString());
+        }
+
+        if (!snapshot.hasData) {
+          return const NoDataView(title: 'Material Detail', message: 'No user data found');
+        }
+
+        final user = snapshot.data!;
+        return Scaffold(
+          appBar: TopBar(
+            companyName: user.companyName,
+            userName: '${user.firstName} ${user.lastName}',
+            actions: [
+              IconButton(
+                icon: Icon(Icons.edit, color: AppStyles.orangeEditIcon.color, size: AppStyles.orangeEditIcon.size),
+                iconSize: AppStyles.topBarIconSize,
+                color: AppStyles.iconTheme.color,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => JobMaterialCreateUpdateView(jobMaterial: jobMaterial),
+                    ),
+                  );
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.delete, color: AppStyles.redDeleteIcon.color, size: AppStyles.redDeleteIcon.size),
+                iconSize: AppStyles.topBarIconSize,
+                color: AppStyles.iconTheme.color,
+                onPressed: () async {
+                  final confirmDelete = await showDialog<bool>(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Center(
+                        child: Text('Delete Material'),
+                      ),
+                      content: Text('Are you sure you want to delete ${jobMaterial.name} from the material list?'),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(false),
+                          child: const Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(true),
+                          child: const Text('Delete'),
+                        ),
+                      ],
+                    ),
+                  );
+
+                  if (confirmDelete == true) {
+                    await jobMaterialService.deleteJobMaterial(jobMaterial.documentId!);
+                    Navigator.pop(context); // Close the detail view after deletion
+                  }
+                },
+              ),
+            ],
+          ),
+          backgroundColor: AppStyles.backgroundColor,
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 8.0), // Reduced spacing
+                  child: Text(
+                    'Material Detail',
+                    style: AppStyles.headlineStyle,
+                  ),
+                ),
+                const SizedBox(height: 8.0), // Reduced spacing
+                Expanded(
+                  child: ListView(
+                    children: [
+                      ReusableCard(
+                        icon: Icons.inventory,
+                        title: 'Name',
+                        subtitle: jobMaterial.name,
+                      ),
+                      ReusableCard(
+                        icon: Icons.attach_money,
+                        title: 'Price',
+                        subtitle: '\$${jobMaterial.price.toStringAsFixed(2)}',
+                      ),
+                      ReusableCard(
+                        icon: Icons.description,
+                        title: 'Description',
+                        subtitle: jobMaterial.description,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          bottomNavigationBar: BottomBar(
+            onHomePressed: navigateHome,
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        );
+      },
     );
   }
 }
